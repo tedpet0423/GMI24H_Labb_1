a3199105c94b04bcf7347541fffdfe2103ee8943
tedpet0423 <h23tedpe@du.se>
2025-04-02 14:25:41 +0200

    vidare arbete

--- CountNumber.cs
+++ CountNumber.cs
@@ -2,7 +2,7 @@ namespace GMI24H_Labb_1;
 
 public class CountNumber
 {
-    public int CountNum(int[] arr, int num)
+    public  int CountNum(int[] arr, int num)
     {
         int count = 0;
         for (int i = 0; i < arr.Length; i++)
--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
 
+using System.Diagnostics;
 
-internal class Program
-{
-    public static void Main(string[] args)
+namespace GMI24H_Labb_1; 
+
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
-        ArrayGen arrayGen = new ArrayGen();
+        public static void Main(string[] args)
+        {
+            int findNum = 42;
+            int arrSize = 100000;
+            int[] testSizes = {10, 10000, 1000000, 1000000000};
+            
+            ArrayGen arrayGen = new ArrayGen();
+            CountNumber countNumber = new CountNumber();
+            
+            
+            for(int i = 0; i < testSizes.Length; i++)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                int[] arr = arrayGen.GenerateArray(testSizes[i], true);
+                
+                stopwatch.Reset();
+                stopwatch.Start();
+                int numCount = countNumber.CountNum(arr, findNum);
+                stopwatch.Stop();
+                
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                
+                Console.WriteLine("Number of " + findNum + "s : " + numCount + " \nSize of array: " + testSizes[i] + " \nTime taken: " + timeSpan.TotalNanoseconds + "ms\n");
+            }
+        }
     }
-}
